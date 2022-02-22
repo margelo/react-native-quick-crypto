04e45167f94c002a52f6faa53782ea30c6b2e39b
Szymon <szymon20000@gmail.com>
2022-02-22 21:12:35 +0100

    feat: add C++ linter triggered on commit

--- cpp/JSICryptoHostObject.cpp
+++ cpp/JSICryptoHostObject.cpp
@@ -1,20 +1,27 @@
+// "Copyright 2022 Margelo
 #include "JSICryptoHostObject.h"
+
 #include <jsi/jsi.h>
+#include <vector>
 
 namespace margelo {
 
-using namespace facebook;
+namespace jsi = facebook::jsi;
 
-// TODO: Create macros for this so we don't have to repeat ourselves for each JSI func?
+// TODO(Szymon): Create macros for this
+// so we don't have to repeat ourselves for each
+// JSI func?
 
-std::vector<jsi::PropNameID> JSICryptoHostObject::getPropertyNames(jsi::Runtime& runtime) {
-    return std::vector<jsi::PropNameID>();
+std::vector<jsi::PropNameID> JSICryptoHostObject::getPropertyNames(
+  jsi::Runtime& runtime) {
+  return std::vector<jsi::PropNameID>();
 }
 
-jsi::Value JSICryptoHostObject::get(jsi::Runtime& runtime, const jsi::PropNameID& propNameId) {
-    auto propName = propNameId.utf8(runtime);
+jsi::Value JSICryptoHostObject::get(jsi::Runtime& runtime,
+                                    const jsi::PropNameID& propNameId) {
+  // auto propName = propNameId.utf8(runtime);
 
-    return jsi::Value::undefined();
+  return jsi::Value::undefined();
 }
 
-} // namespace margelo
+}  // namespace margelo
