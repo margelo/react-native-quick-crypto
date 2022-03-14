9fbdaa419104738524e4aacc67d4aa29a47340fc
Szymon <szymon20000@gmail.com>
2022-03-14 14:55:30 +0100

    add tests

--- example/src/RandomTests/random.ts
+++ example/src/RandomTests/random.ts
@@ -1,6 +1,8 @@
-import { FastCrypto as crypto } from 'react-native-fact-crypto';
+import { FastCrypto as crypto } from 'react-native-fast-crypto';
 import { describe, it } from '../MochaRNAdapter';
 import chai from 'chai';
+
+const assert = chai.assert;
 
 export function registerRandomTests() {
   
