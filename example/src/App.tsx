99d87ec507420640943174b95d27a29bda085ca8
Oscar Franco <ospfranco@protonmail.com>
2022-06-06 12:38:19 +0200

    Clean up main App.tsx file

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,38 +1,6 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
 import { Root } from './navigators/Root';
 
 export default function App() {
   return <Root />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-  },
-  keys: {
-    fontSize: 14,
-    color: 'grey',
-  },
-  title: {
-    fontSize: 16,
-    color: 'black',
-    marginRight: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  textInput: {
-    flex: 1,
-    marginVertical: 20,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: 'black',
-    borderRadius: 5,
-    padding: 10,
-  },
-});
