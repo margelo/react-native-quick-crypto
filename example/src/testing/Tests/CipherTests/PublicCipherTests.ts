8b59820576e6b4069387a3337617b2c8c2717127
Oscar Franco <ospfranco@protonmail.com>
2022-06-29 08:49:31 +0200

    clean-up add public cipher tests

--- example/src/testing/Tests/CipherTests/PublicCipherTests.ts
+++ example/src/testing/Tests/CipherTests/PublicCipherTests.ts
@@ -3,6 +3,42 @@ import { Buffer } from '@craftzdog/react-native-buffer';
 import { it } from '../../MochaRNAdapter';
 import { QuickCrypto as crypto } from 'react-native-quick-crypto';
 
+// Tests that a key pair can be used for encryption / decryption.
+function testEncryptDecrypt(publicKey: any, privateKey: any) {
+  const message = 'Hello Node.js world!';
+  const plaintext = Buffer.from(message, 'utf8');
+  for (const key of [publicKey, privateKey]) {
+    const ciphertext = crypto.publicEncrypt(key, plaintext);
+    const received = crypto.privateDecrypt(privateKey, ciphertext);
+    chai.assert.strictEqual(received.toString('utf8'), message);
+  }
+}
+
+// I guess interally this functions use privateEncrypt/publicDecrypt (sign/verify)
+// but the main function `sign` is not implemented yet
+// Tests that a key pair can be used for signing / verification.
+// function testSignVerify(publicKey: any, privateKey: any) {
+//   const message = Buffer.from('Hello Node.js world!');
+
+//   function oldSign(algo, data, key) {
+//     return createSign(algo).update(data).sign(key);
+//   }
+
+//   function oldVerify(algo, data, key, signature) {
+//     return createVerify(algo).update(data).verify(key, signature);
+//   }
+
+//   for (const signFn of [sign, oldSign]) {
+//     const signature = signFn('SHA256', message, privateKey);
+//     for (const verifyFn of [verify, oldVerify]) {
+//       for (const key of [publicKey, privateKey]) {
+//         const okay = verifyFn('SHA256', message, key, signature);
+//         assert(okay);
+//       }
+//     }
+//   }
+// }
+
 export function registerPublicCipherTests() {
   // We need to monkey patch sscrypto to use all the crypto functions from quick-crypto
   // it('sscrypto basic test', async () => {
@@ -32,6 +68,22 @@ export function registerPublicCipherTests() {
 
   it('publicEncrypt/privateDecrypt', () => {
     const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 512,
+      publicKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+      },
+    });
+
+    testEncryptDecrypt(publicKey, privateKey);
+  });
+
+  it('publicEncrypt/privateDecrypt with non-common exponent', () => {
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
       publicExponent: 3,
       modulusLength: 512,
       publicKeyEncoding: {
@@ -44,12 +96,7 @@ export function registerPublicCipherTests() {
       },
     });
 
-    const message = 'Hello RN world!';
-    const plaintext = Buffer.from(message, 'utf8');
-    const ciphertext = crypto.publicEncrypt(publicKey, plaintext);
-    const decrypted = crypto.privateDecrypt(privateKey, ciphertext);
-
-    chai.expect(decrypted.toString('utf-8')).to.equal(message);
+    testEncryptDecrypt(publicKey, privateKey);
   });
 
   it('publicEncrypt/privateDecrypt with passphrase', () => {
@@ -76,5 +123,28 @@ export function registerPublicCipherTests() {
     );
 
     chai.expect(decrypted.toString('utf-8')).to.equal(message);
+  });
+
+  it('passphrased private key without passphrase should throw', () => {
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'top secret',
+      },
+    });
+
+    try {
+      testEncryptDecrypt(publicKey, privateKey);
+      chai.assert.fail();
+    } catch (e) {
+      // intentionally left blank
+    }
   });
 }
