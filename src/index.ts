ce3144b9f8b97b6dcc76d82d13a09abd6d3595a2
Szymon Kapała <szymon20000@gmail.com>
2022-07-19 13:56:06 +0200

    fix randombytes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import { Buffer } from '@craftzdog/react-native-buffer';
 import { QuickCrypto } from './QuickCrypto';
+
+global.Buffer = Buffer;
+global.crypto = QuickCrypto; // for randombytes https://github.com/crypto-browserify/randombytes/blob/master/browser.js#L16
+
 const fallbackCrypto = require('crypto-browserify');
-const crypto = {...fallbackCrypto, ...QuickCrypto};
+const crypto = {...fallbackCrypto, ...QuickCrypto}; // Maybe use proxy to not load everything?
+global.crypto = crypto;
 
 module.exports = crypto;
 export default crypto;
