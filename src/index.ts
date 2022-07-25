f0dda4050e0df1599652216ff8d79994c294c1c3
Marc Rousavy <me@mrousavy.com>
2022-07-25 11:43:04 +0200

    fix: TEMPORARY FIX: ts-ignore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Buffer } from '@craftzdog/react-native-buffer';
 import { QuickCrypto } from './QuickCrypto';
 
+// @ts-expect-error
 global.Buffer = Buffer;
+// @ts-expect-error
 global.crypto = QuickCrypto; // for randombytes https://github.com/crypto-browserify/randombytes/blob/master/browser.js#L16
 
 const fallbackCrypto = require('crypto-browserify');
-const crypto = {...fallbackCrypto, ...QuickCrypto}; // Maybe use proxy to not load everything?
+const crypto = { ...fallbackCrypto, ...QuickCrypto }; // Maybe use proxy to not load everything?
 global.crypto = crypto;
 
 module.exports = crypto;
