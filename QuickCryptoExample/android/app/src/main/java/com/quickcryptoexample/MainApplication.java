e01ce38e14f08b06dfce83c566ae179113926758
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-04-06 07:25:10 +0200

    android: make package work

--- QuickCryptoExample/android/app/src/main/java/com/quickcryptoexample/MainApplication.java
+++ QuickCryptoExample/android/app/src/main/java/com/quickcryptoexample/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.margelo.quickcrypto.QuickCryptoPackage;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -25,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new QuickCryptoPackage());
           return packages;
         }
 
