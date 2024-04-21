29999e95303df90d5d5e264a477f32574fcd64ad
Timothy Shamilov <shamilovtim@gmail.com>
2024-04-21 13:00:46 -0400

    feat: add getHashes. add getCiphers. remove crypto-browserify. (#264)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { Buffer } from '@craftzdog/react-native-buffer';
 import { QuickCrypto } from './QuickCrypto';
-import FallbackCrypto from 'crypto-browserify';
 
 // @ts-expect-error Buffer does not match exact same type definition.
 global.Buffer = Buffer;
 
-const crypto = { ...FallbackCrypto, ...QuickCrypto };
-
-// for randombytes https://github.com/crypto-browserify/randombytes/blob/master/browser.js#L16
-// @ts-expect-error QuickCrypto is missing `subtle` and `randomUUID`
-global.crypto = crypto;
+// @ts-expect-error subtle isn't full implemented and Cryptokey is missing
+global.crypto = QuickCrypto;
 
 module.exports = crypto;
 export default crypto;
