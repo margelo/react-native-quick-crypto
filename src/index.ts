5aee01dce6ef80e4819500a1b0a5d0468aa08b42
Brad Anderson <brad@sankatygroup.com>
2024-04-23 14:10:38 -0400

    fix: cleanup items for release candidate (#273)

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,4 @@ global.Buffer = Buffer;
 // @ts-expect-error subtle isn't full implemented and Cryptokey is missing
 global.crypto = QuickCrypto;
 
-module.exports = crypto;
-export default crypto;
+export default QuickCrypto;
