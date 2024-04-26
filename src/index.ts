9f668c39d97981b2355560af29e77e71994eaaac
Timothy Shamilov <shamilovtim@gmail.com>
2024-04-26 13:40:13 -0400

    fix: broken cjs export (#279)

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@ global.Buffer = Buffer;
 // @ts-expect-error subtle isn't full implemented and Cryptokey is missing
 global.crypto = QuickCrypto;
 
+module.exports = QuickCrypto;
 export default QuickCrypto;
