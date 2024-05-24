75a848e2b12f4bf30a14f6808f137f2c072d9192
Szymon Kapała <szymon20000@gmail.com>
2024-05-24 15:39:08 +0200

    stash work

--- android/src/main/cpp/cpp-adapter.cpp
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -3,58 +3,20 @@
 #include <jni.h>
 #include <jsi/jsi.h>
 
-#include "MGLQuickCryptoHostObject.h"
+#import <React/RCTUtils.h>
+#import <ReactCommon/RCTTurboModule.h>
+#import "RNTQuickCryptoCxx.h"
+#import <ReactCommon/CxxTurboModuleUtils.h>
 
 using namespace facebook;
 
-class CryptoCppAdapter : public jni::HybridClass<CryptoCppAdapter> {
- public:
-  static auto constexpr kJavaDescriptor =
-      "Lcom/margelo/quickcrypto/QuickCryptoModule;";
 
-  static jni::local_ref<jni::HybridClass<CryptoCppAdapter>::jhybriddata>
-  initHybrid(jni::alias_ref<jhybridobject> jThis) {
-    return makeCxxInstance();
-  }
-
-  CryptoCppAdapter() {}
-
-  void install(jsi::Runtime &runtime,
-               std::shared_ptr<facebook::react::CallInvoker> jsCallInvoker) {
-    auto workerQueue = std::make_shared<margelo::DispatchQueue::dispatch_queue>(
-        "margelo crypto worker thread");
-    auto hostObject = std::make_shared<margelo::MGLQuickCryptoHostObject>(
-        jsCallInvoker, workerQueue);
-    auto object = jsi::Object::createFromHostObject(runtime, hostObject);
-    runtime.global().setProperty(runtime, "__QuickCryptoProxy",
-                                 std::move(object));
-  }
-
-  void nativeInstall(
-      jlong jsiPtr,
-      jni::alias_ref<facebook::react::CallInvokerHolder::javaobject>
-          jsCallInvokerHolder) {
-    auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
-    auto runtime = reinterpret_cast<jsi::Runtime *>(jsiPtr);
-    if (runtime) {
-      install(*runtime, jsCallInvoker);
-    }
-    // if runtime was nullptr, QuickCrypto will not be installed. This should
-    // only happen while Remote Debugging (Chrome), but will be weird either
-    // way.
-  }
-
-  static void registerNatives() {
-    registerHybrid(
-        {makeNativeMethod("initHybrid", CryptoCppAdapter::initHybrid),
-         makeNativeMethod("nativeInstall", CryptoCppAdapter::nativeInstall)});
-  }
-
- private:
-  friend HybridBase;
-};
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
-  return facebook::jni::initialize(vm,
-                                   [] { CryptoCppAdapter::registerNatives(); });
+  facebook::react::registerCxxModuleToGlobalModuleMap(
+                                                        std::string(facebook::react::margelo::RNTQuickCryptoCxx::kModuleName),
+                                                        [&](std::shared_ptr<facebook::react::CallInvoker> jsInvoker) {
+                                                            return std::make_shared<facebook::react::margelo::RNTQuickCryptoCxx>(jsInvoker);
+                                                        });
+  return JNI_VERSION_1_6;
 }
