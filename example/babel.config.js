579f87dd19051afcd9e9edb5049d3f085f83fc32
Brad Anderson <brad@sankatygroup.com>
2024-08-08 16:12:19 -0400

    add Benchmarks scaffolding

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
   plugins: [
     ['@babel/plugin-transform-class-static-block'],
     [
