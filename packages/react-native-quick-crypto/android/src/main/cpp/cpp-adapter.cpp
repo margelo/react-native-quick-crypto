9938341f1fc018e831e2333e39dd20c6b0b6c9c3
Brad Anderson <brad@sankatygroup.com>
2024-08-11 00:01:40 -0400

    fix eslint rig, more gha

--- packages/react-native-quick-crypto/android/src/main/cpp/cpp-adapter.cpp
+++ packages/react-native-quick-crypto/android/src/main/cpp/cpp-adapter.cpp
@@ -1,9 +1,5 @@
-#include <ReactCommon/CallInvokerHolder.h>
 #include <fbjni/fbjni.h>
 #include <jni.h>
-#include <jsi/jsi.h>
-
-#include "MGLQuickCryptoHostObject.h"
 
 using namespace facebook;
 
@@ -16,24 +12,6 @@ public:
   }
 
   CryptoCppAdapter() {}
-
-  void install(jsi::Runtime& runtime, std::shared_ptr<facebook::react::CallInvoker> jsCallInvoker) {
-    auto workerQueue = std::make_shared<margelo::DispatchQueue::dispatch_queue>("margelo crypto worker thread");
-    auto hostObject = std::make_shared<margelo::MGLQuickCryptoHostObject>(jsCallInvoker, workerQueue);
-    auto object = jsi::Object::createFromHostObject(runtime, hostObject);
-    runtime.global().setProperty(runtime, "__QuickCryptoProxy", std::move(object));
-  }
-
-  void nativeInstall(jlong jsiPtr, jni::alias_ref<facebook::react::CallInvokerHolder::javaobject> jsCallInvokerHolder) {
-    auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
-    auto runtime = reinterpret_cast<jsi::Runtime*>(jsiPtr);
-    if (runtime) {
-      install(*runtime, jsCallInvoker);
-    }
-    // if runtime was nullptr, QuickCrypto will not be installed. This should
-    // only happen while Remote Debugging (Chrome), but will be weird either
-    // way.
-  }
 
   static void registerNatives() {
     registerHybrid(
