238fe7d30b62f4074c27e7a4675e521a36ceef98
Brad Anderson <brad@fragile.co>
2024-09-08 11:12:06 -0400

    gha bumps, fix delete lambda

--- packages/react-native-quick-crypto/cpp/random/HybridRandom.cpp
+++ packages/react-native-quick-crypto/cpp/random/HybridRandom.cpp
@@ -8,25 +8,35 @@ namespace margelo::crypto {
 using namespace margelo::nitro;
 using namespace margelo::nitro::crypto;
 
-std::future<std::shared_ptr<ArrayBuffer>> HybridRandom::randomFill(const std::shared_ptr<ArrayBuffer>& buffer, double dOffset,
-                                                                   double dSize) {
+std::future<std::shared_ptr<ArrayBuffer>>
+HybridRandom::randomFill(const std::shared_ptr<ArrayBuffer>& buffer,
+                         double dOffset,
+                         double dSize) {
   size_t size = checkSize(dSize);
-  // copy the JSArrayBuffer that we do not own into a NativeArrayBuffer that we do own, before passing to sync function
+  // copy the JSArrayBuffer that we do not own into a NativeArrayBuffer that we
+  // do own, before passing to sync function
   uint8_t* data = new uint8_t[size];
   memcpy(data, buffer.get()->data(), size);
-  std::shared_ptr<ArrayBuffer> nativeBuffer = std::make_shared<NativeArrayBuffer>(data, size, nullptr);
+  std::shared_ptr<ArrayBuffer> nativeBuffer =
+    std::make_shared<NativeArrayBuffer>(data, size, [=]() { delete[] data; });
 
   return std::async(std::launch::async,
-                    [this, nativeBuffer, dOffset, dSize]() { return this->randomFillSync(nativeBuffer, dOffset, dSize); });
+                    [this, nativeBuffer, dOffset, dSize]() {
+                      return this->randomFillSync(nativeBuffer, dOffset, dSize);
+                    });
 };
 
-std::shared_ptr<ArrayBuffer> HybridRandom::randomFillSync(const std::shared_ptr<ArrayBuffer>& buffer, double dOffset, double dSize) {
+std::shared_ptr<ArrayBuffer>
+HybridRandom::randomFillSync(const std::shared_ptr<ArrayBuffer>& buffer,
+                             double dOffset,
+                             double dSize) {
   size_t size = checkSize(dSize);
   size_t offset = checkOffset(dSize, dOffset);
   uint8_t* data = buffer.get()->data();
 
   if (RAND_bytes(data + offset, (int)size) != 1) {
-    throw std::runtime_error("error calling RAND_bytes" + std::to_string(ERR_get_error()));
+    throw std::runtime_error("error calling RAND_bytes" +
+      std::to_string(ERR_get_error()));
   }
   return std::make_shared<NativeArrayBuffer>(data, size, nullptr);
 };
