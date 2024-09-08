0fe50a1236154a24394185e844ab89df4b98d7f9
Brad Anderson <brad@fragile.co>
2024-09-08 10:46:27 -0400

    chore: bump nitro to 0.7.0, patch rnscreens, java22 & gradle 8.8

--- packages/react-native-quick-crypto/cpp/random/HybridRandom.cpp
+++ packages/react-native-quick-crypto/cpp/random/HybridRandom.cpp
@@ -14,7 +14,7 @@ std::future<std::shared_ptr<ArrayBuffer>> HybridRandom::randomFill(const std::sh
   // copy the JSArrayBuffer that we do not own into a NativeArrayBuffer that we do own, before passing to sync function
   uint8_t* data = new uint8_t[size];
   memcpy(data, buffer.get()->data(), size);
-  std::shared_ptr<ArrayBuffer> nativeBuffer = std::make_shared<NativeArrayBuffer>(data, size, false);
+  std::shared_ptr<ArrayBuffer> nativeBuffer = std::make_shared<NativeArrayBuffer>(data, size, nullptr);
 
   return std::async(std::launch::async,
                     [this, nativeBuffer, dOffset, dSize]() { return this->randomFillSync(nativeBuffer, dOffset, dSize); });
@@ -28,7 +28,7 @@ std::shared_ptr<ArrayBuffer> HybridRandom::randomFillSync(const std::shared_ptr<
   if (RAND_bytes(data + offset, (int)size) != 1) {
     throw std::runtime_error("error calling RAND_bytes" + std::to_string(ERR_get_error()));
   }
-  return std::make_shared<NativeArrayBuffer>(data, size, false);
+  return std::make_shared<NativeArrayBuffer>(data, size, nullptr);
 };
 
 } // namespace margelo::crypto
