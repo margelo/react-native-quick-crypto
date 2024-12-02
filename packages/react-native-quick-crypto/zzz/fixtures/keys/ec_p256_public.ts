084d2c232a4c16e31b29459421cc02d35a8c8d8e
Brad Anderson <brad@sankatygroup.com>
2024-12-02 12:34:45 -0500

    chore: zap the old code (#535)

--- packages/react-native-quick-crypto/zzz/fixtures/keys/ec_p256_public.ts
+++
@@ -1,8 +0,0 @@
-const key: string = `
------BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEX0mMYR/uleZSIPjNztIkAS3/ud5L
-hNpbiIFp6fNf2GtRsm4/LZeLSVbtRhMHE8rfIaAO70QApgh5yOQdfmzaUg==
------END PUBLIC KEY-----
-`
-
-export default key
--- packages/react-native-quick-crypto/zzz/ts/NativeQuickCrypto/hmac.ts
+++
@@ -1,9 +0,0 @@
-export type InternalHmac = {
-  update: (data: ArrayBuffer) => InternalHmac;
-  digest: () => ArrayBuffer;
-};
-
-export type CreateHmacMethod = (
-  algorithm: string,
-  key?: ArrayBuffer
-) => InternalHmac;
