5984b4ddaadf7082df77c287db4f2f8d6e2e1f19
Brad Anderson <brad@sankatygroup.com>
2025-01-01 21:25:17 -0500

    chore: release 0.7.11

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting the release process..."
+echo "Provided options: $@"
+
+echo "Publishing 'react-native-quick-crypto' to NPM"
+cp README.md packages/react-native-quick-crypto/README.md
+cd packages/react-native-quick-crypto
+bun release $@
+
+echo "Creating a Git bump commit and GitHub release"
+cd ../..
+bun run release-it $@
+
+echo "\nSuccessfully released QuickCrypto!"
