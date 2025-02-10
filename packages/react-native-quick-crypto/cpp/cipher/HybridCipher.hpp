a953f1c9026710ddbaf5005e0ef0d85a00f4fe66
Brad Anderson <brad@sankatygroup.com>
2025-02-09 23:37:11 -0500

    separate init functions for the problematic modes

--- packages/react-native-quick-crypto/cpp/cipher/HybridCipher.hpp
+++ packages/react-native-quick-crypto/cpp/cipher/HybridCipher.hpp
@@ -71,12 +71,26 @@ class HybridCipher : public HybridCipherSpec {
   bool initAuthenticated(
     const char *cipher_type,
     int iv_len,
-    unsigned int auth_tag_len
+    unsigned int auth_tag_len,
+    const std::shared_ptr<NativeArrayBuffer>& native_iv
   );
 
   bool maybePassAuthTagToOpenSSL();
 
   bool checkCCMMessageLength(int message_len);
+
+  bool initCCMMode(
+    int iv_len,
+    const std::shared_ptr<NativeArrayBuffer>& native_iv
+  );
+  bool initGCMMode();
+  bool initOCBMode(const std::shared_ptr<NativeArrayBuffer>& native_iv);
+  bool initSIVMode();
+  bool initGCMSIVMode();
+  bool initChaCha20Poly1305();
+
+  // Helper function to set authentication tag length
+  bool setAuthTagLength(const char* mode_str);
 
   int getMode();
 
@@ -85,7 +99,8 @@ class HybridCipher : public HybridCipherSpec {
   bool is_cipher = true;
   std::string cipher_type;
   EVP_CIPHER_CTX *ctx = nullptr;
-  bool pending_auth_failed;
+  bool pending_auth_failed = false;
+  bool has_aad = false;
   uint8_t auth_tag[EVP_GCM_TLS_TAG_LEN];
   AuthTagState auth_tag_state;
   unsigned int auth_tag_len;
