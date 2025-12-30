50811e77dd2e010064169ad601ca25a2ffd45862
Brad Anderson <brad@sankatygroup.com>
2025-12-30 09:11:05 -0600

    chore: deploy docs (#870)

--- docs/app/api/search/route.ts
+++ docs/app/api/search/route.ts
@@ -1,7 +1,5 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
-export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/docs/orama-js/supported-languages
-  language: 'english',
-});
+export const revalidate = false;
+export const { staticGET: GET } = createFromSource(source);
