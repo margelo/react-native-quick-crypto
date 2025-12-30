d0a35caf3da648ccd1a3a87ced5511108a8e4ddb
Brad Anderson <brad@sankatygroup.com>
2025-12-30 10:15:01 -0500

    fix: gh pages paths

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,10 +2,14 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   output: 'export',
+  basePath: isGitHubPages ? '/react-native-quick-crypto' : '',
+  assetPrefix: isGitHubPages ? '/react-native-quick-crypto/' : '',
 };
 
 export default withMDX(config);
